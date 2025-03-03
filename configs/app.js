04d66ca9d849bbd99847ea687066e1c25f5ea63f
jpirir <jpirir-2020285@kinal.edu.gt>
2025-03-03 08:49:57 -0600

    Se agregaron las rutas en app, para poder ejecutar

--- configs/app.js
+++ configs/app.js
@@ -8,6 +8,8 @@ import userRoutes from '../src/user/user.routes.js'
 import productRoutes from '../src/product/product.routes.js'
 import categoryRoutes from "../src/category/category.routes.js"
 import authRoutes from '../src/auth/auth.routes.js'
+import cartRoutes from '../src/cart/cart.routes.js'
+import invoiceRoutes from '../src/invoice/invoice.routes.js'
 import { createDefaultAdmin } from '../src/user/user.controller.js'
 import { limiter } from '../middlewares/rate.limit.js'
 
@@ -26,6 +28,8 @@ const routes = (app)=>{
     app.use('/v1/product',productRoutes)
     app.use('/v1/category', categoryRoutes)
     app.use('/v1/auth', authRoutes)
+    app.use('/v1/cart', cartRoutes)
+    app.use('/v1/invoice', invoiceRoutes)
 }
 
 export const initServer = async()=>{
