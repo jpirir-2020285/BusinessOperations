d45a5e85d3e9be4a96eb5d3d9eceb8991f5f99d1
jpirir <jpirir-2020285@kinal.edu.gt>
2025-03-03 08:46:39 -0600

    Se agrega modelo de carrito

---
+++ src/cart/cart.model.js
@@ -0,0 +1,32 @@
+import { Schema, model } from 'mongoose';
+
+const cartSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        products: [
+            {
+                product: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Product',
+                    required: true
+                },
+                quantity: {
+                    type: Number,
+                    required: true,
+                    min: [1, 'Quantity must be at least 1']
+                }
+            }
+        ],
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    { timestamps: true }
+);
+
+export default model('Cart', cartSchema);
